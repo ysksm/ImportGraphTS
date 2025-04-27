6b42c642743fe6a9a813684a3cf32ebabba6591f
kasama <ka.sa.ma.123@gmail.com>
2025-04-28 06:45:01 +0900

    edit graph-view

---
+++ graph-view/src/App.tsx
@@ -0,0 +1,8 @@
+// src/App.tsx
+import React from 'react';
+import GraphView from './GraphView';
+import '@xyflow/react/dist/style.css';
+
+const App: React.FC = () => <GraphView />;
+
+export default App;
